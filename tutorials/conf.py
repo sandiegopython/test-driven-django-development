41dc8b824c53ca52a0430786a0fb07595694465a
David Fischer <djfische@gmail.com>
2014-02-24 21:54:37 -0800

    2014 :sparkles:

--- tutorials/conf.py
+++ tutorials/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Test-Driven Django Development'
-copyright = u'2013, San Diego Python'
+copyright = u'2014, San Diego Python'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
